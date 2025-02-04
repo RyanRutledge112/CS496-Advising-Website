13f242e514973682918752ad181eee843f095329
delaneyflaherty <128857617+delaneyflaherty@users.noreply.github.com>
2025-02-04 12:23:54 -0600

    Update views.py for login

--- Advising Website/AdvisingWebsiteFiles/advisingwebsite/advisingwebsiteapp/views.py
+++ Advising Website/AdvisingWebsiteFiles/advisingwebsite/advisingwebsiteapp/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html', {})
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)  # Log in
+            return redirect('home')  # Redirect to homepage
+        else:
+            messages.error(request, "Invalid email or password")  # Show error message
+    return render(request, 'login.html')
