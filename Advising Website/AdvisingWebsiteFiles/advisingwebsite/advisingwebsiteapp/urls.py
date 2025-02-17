fcf46f7feb26024e850d2330545aebd6a72af900
delaneyflaherty <128857617+delaneyflaherty@users.noreply.github.com>
2025-02-17 15:49:13 -0600

    Update urls.py for messages and logging out

--- Advising Website/AdvisingWebsiteFiles/advisingwebsite/advisingwebsiteapp/urls.py
+++ Advising Website/AdvisingWebsiteFiles/advisingwebsite/advisingwebsiteapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.login_view, name='login'), #login page route
     path('register/', views.register, name="register"), #register page route
-    path('features/', views.features, name="features"), #features page route
     path('messages/', views.messages, name="messages"), #messages page route
+    path('send_message/', views.send_message, name="send_message"),
     path('uploadTranscript/', views.upload_transcript, name="uploadTranscript"), #upload transcript page
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'), #logout returns to home(signed out page)
 ]
