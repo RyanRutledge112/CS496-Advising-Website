860063896b1be32471dea8e31b5df3c5297d6e58
Ryan Rutledge <ryan.rutledge325@topper.wku.edu>
2025-02-26 18:37:40 -0600

    Showing users inside admin side of website

--- Advising Website/AdvisingWebsiteFiles/advisingwebsite/advisingwebsiteapp/admin.py
+++ Advising Website/AdvisingWebsiteFiles/advisingwebsite/advisingwebsiteapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Chat, ChatMember, Message, Degree, DegreeCourse, Course, Prerequisites
+from .models import User, Chat, ChatMember, Message, Degree, DegreeCourse, Course, Prerequisites, UserDegree
 
 # Register your models here.
 
+admin.site.register(User)
 admin.site.register(Chat)
 admin.site.register(ChatMember)
 admin.site.register(Message)
@@ -10,3 +11,4 @@ admin.site.register(Degree)
 admin.site.register(DegreeCourse)
 admin.site.register(Course)
 admin.site.register(Prerequisites)
+admin.site.register(UserDegree)
