045cb8c6e5a8d49abfd277e18f5349e7c1947648
Ryan Rutledge <ryan.rutledge325@topper.wku.edu>
2025-04-15 13:50:02 -0500

    Cross platform testing

--- Advising Website/AdvisingWebsiteFiles/advisingwebsite/run_tests.py
+++ Advising Website/AdvisingWebsiteFiles/advisingwebsite/run_tests.py
@@ -1,11 +1,20 @@
 import subprocess
+import os
+import platform
 
 def run_tests():
     # Run Django tests with pytest
     subprocess.run(['pytest', '--maxfail=1', '--disable-warnings', '-q'])
+    
+    system = platform.system()
 
-    # Run Jest tests for JavaScript
-    subprocess.run([r'C:\Program Files\nodejs\npm.cmd', 'test', '--', '--maxWorkers=2'])
+    if system == 'Windows':
+        # Full path to npm.cmd on Windows
+        npm_cmd = r'C:\Program Files\nodejs\npm.cmd'
+        subprocess.run([npm_cmd, 'test', '--', '--maxWorkers=2'])
+    else:
+        # macOS/Linux – just use 'npm' from PATH
+        subprocess.run(['npm', 'test', '--', '--maxWorkers=2'])
 
 if __name__ == '__main__':
     run_tests()
